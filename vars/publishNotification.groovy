8fc1a13090ce726a741b1695a05a9a2ffaa3a318
pgodithi <pgodithi@amazon.com>
2022-03-16 05:30:49 -0700

    Added new webhook url
    Signed-off-by: pgodithi <pgodithi@amazon.com>

--- vars/publishNotification.groovy
+++ vars/publishNotification.groovy
@@ -1,11 +1,13 @@
 void call(Map args = [:]) {
     text = ([
-        "${args.icon} ${JOB_NAME} [${BUILD_NUMBER}] ${args.message}",
+        "${args.icon}", 
+        "JOB_NAME=${JOB_NAME}",
+        "BUILD_NUMBER=[${BUILD_NUMBER}]",
+        "MESSAGE=${args.message}",
         "Build: ${BUILD_URL}",
         args.manifest,
         args.extra
     ] - null).join("\n")
-
     withCredentials([string(credentialsId: args.credentialsId, variable: 'WEBHOOK_URL')]) {
         sh ([
             'curl',
@@ -16,3 +18,4 @@ void call(Map args = [:]) {
         ].join(' '))
     }
 }
+
