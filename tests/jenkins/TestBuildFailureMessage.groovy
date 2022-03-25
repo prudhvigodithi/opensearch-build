40083f90eeddfbe70945bac48ab155af7b8fd114
pgodithi <pgodithi@amazon.com>
2022-03-25 08:29:52 -0700

    Added data types
    Signed-off-by: pgodithi <pgodithi@amazon.com>

---
+++ tests/jenkins/TestBuildFailureMessage.groovy
@@ -0,0 +1,26 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package jenkins.tests
+import jenkins.tests.BuildPipelineTest
+import org.junit.*
+
+
+class TestBuildFailureMessage extends BuildPipelineTest {
+
+    @Before
+    void setUp() {
+        this.registerLibTester(new BuildFailureMessageLibTester())
+        super.setUp()
+    }
+
+    @Test
+    void testSignArtifacts() {
+        super.testPipeline("jobs/BuildFailureMessage_Jenkinsfile")
+    }
+}
