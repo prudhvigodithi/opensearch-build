3a767ba5c5bb930c95d86481b43cc1e1ef30e38f
pgodithi <pgodithi@amazon.com>
2022-03-25 06:46:20 -0700

    Added data types
    Signed-off-by: pgodithi <pgodithi@amazon.com>

--- vars/publishNotification.groovy
+++ vars/publishNotification.groovy
@@ -1,12 +1,11 @@
 void call(Map args = [:]) {
     text = ([
         "${args.icon}", 
-        "JOB_NAME=${JOB_NAME}",
-        "BUILD_NUMBER=[${BUILD_NUMBER}]",
-        "MESSAGE=${args.messages}",
-        "BUILD_URL: ${BUILD_URL}",
-        "MANIFEST: ${args.manifest}",
-        args.extra
+        "*JOB_NAME*=${JOB_NAME}",
+        "*BUILD_NUMBER*=[${BUILD_NUMBER}]",
+        "*MESSAGE*=${args.messages}",
+        "*BUILD_URL*: ${BUILD_URL}",
+        "*MANIFEST*: ${args.manifest}", args.extra
     ] - null).join("\n")
     withCredentials([string(credentialsId: args.credentialsId, variable: 'WEBHOOK_URL')]) {
         sh ([
